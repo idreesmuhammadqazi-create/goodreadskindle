0b173c62896498aff64d33214bc22dd652b784d3
idreesmuhammadqazi-create <idreesmuhammadqazi@gmail.com>
2026-09-18 09:52:41 +0000

    added link na dauthor name extraction and changed title extraction to be from <script> tags

--- converttojson.py
+++ converttojson.py
@@ -1,17 +1,26 @@
 from bs4 import BeautifulSoup
+data = []
+
 with open("page.html") as e:
     html = BeautifulSoup(e.read(), "html.parser")
 
 recswithads = html.find(class_="recsListing")
-rectitles = recswithads.select('a[href^="/book/show"]')
-rawrecdescriptions = recswithads.find(class_="bookTitle")
+for s in html.find_all("script"):
+    if "bookCover" not in (s.string or '') :
+        pass
+    else :
+        with open("tmp.txt", "w" ) as t:
+            t.write(s.string)
+        
+        with open("tmp.txt", "r") as t:
+            text = t.read()
+            rawtitle = text.split("<h2>")[1].split(r"<\/h2>")[0]
+            link = rawtitle.split(r'href=\"')[1].split(r'">')[0]
+            title = rawtitle.split(r'false\">')[1].split(r'<\/a>')[0]
+            author = text.split(r"origin=recs_landing\">")[1].split(r'<\/a>')[0]
+            row = [title , author , link]
+            data.append(row)
+            
+            
 
-titles = []
-prefixlink = "goodreads.com"
-for i in rectitles:
-    link = str(i.get("href"))
-    link = prefixlink + link
-    image = i.find("img")
-    name = image.get("alt")
-    titles.append([name , link])
-print(titles)
+print(data)
