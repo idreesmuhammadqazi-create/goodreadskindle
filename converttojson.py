d239ce40cb42057bc50c1962f24eede7734650da
idreesmuhammadqazi-create <idreesmuhammadqazi@gmail.com>
2026-09-19 06:55:55 +0000

    added deduplication for books

--- converttojson.py
+++ converttojson.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
+import html
 data = []
+exists = set()
 
 with open("page.html") as e:
-    html = BeautifulSoup(e.read(), "html.parser")
+    page = BeautifulSoup(e.read(), "html.parser")
 
-recswithads = html.find(class_="recsListing")
-for s in html.find_all("script"):
+recswithads = page.find(class_="recsListing")
+for s in page.find_all("script"):
     if "bookCover" not in (s.string or '') :
         pass
     else :
@@ -15,11 +17,17 @@ for s in html.find_all("script"):
         with open("tmp.txt", "r") as t:
             text = t.read()
             rawtitle = text.split("<h2>")[1].split(r"<\/h2>")[0]
-            link = rawtitle.split(r'href=\"')[1].split(r'">')[0]
+            link = rawtitle.split(r'href=\"')[1].split(r'\">')[0]
+            link = link.split("?")[0]
+            bookid = link.split(r"book/show/")[1].split(r"-")[0]
             title = rawtitle.split(r'false\">')[1].split(r'<\/a>')[0]
+            title = html.unescape(title)
             author = text.split(r"origin=recs_landing\">")[1].split(r'<\/a>')[0]
-            row = [title , author , link]
-            data.append(row)
+            if bookid in exists :
+                pass
+            else:
+                row = [title , author , link]
+                data.append(row)
             
             
 
